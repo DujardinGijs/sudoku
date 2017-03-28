70148c15662656120b2c3494bdbd80d6dd36fd51
JensVD <jensvandale@outlook.com>
2017-03-28 13:29:13 +0200

    added check button & implemented timer

---
+++ SudokuWebPage/assets/js/javascript.js
@@ -0,0 +1,25 @@
+var time = 0;
+var clicked = false;
+
+var tel = function (e) {
+    e.preventDefault();
+    if(clicked == false) {
+        i = window.setInterval(count, 1000);
+        clicked = true;
+    }
+};
+
+var count = function () {
+    time++;
+    var min = parseInt(time / 60);
+    var sec = time % 60;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+    var result = "Time: " + min + ":"+ sec;
+    $("#counter").empty();
+    $("#counter").append(result);
+};
+
+$(document).ready(function () {
+    $(".counter").on('click', tel);
+});
